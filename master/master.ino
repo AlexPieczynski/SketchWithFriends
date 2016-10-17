bf01bcbe58af417311e8533c3ff6d0c3bbfad459
Alex Pieczynski <apiecz2@uic.edu>
2016-10-17 13:10:52 -0500

    added master/slave sketches with basic descriptions

---
+++ master/master.ino
@@ -0,0 +1,20 @@
+/*
+ * This is the code that will run on the master arduino
+ * The master will:
+ *  -accept input from all slaves
+ *  -draw to an LCD screen using this input
+ *  
+ *  Created by Alex Pieczynski, Ryan Fogarty, and Diego Vega
+ */
+
+#define USR1 1
+#define USR2 2
+
+void setup()
+{
+
+}
+
+void loop()
+{
+}
---
+++ slave/slave.ino
@@ -0,0 +1,20 @@
+/*
+ * This is the code that will run on the slave arduinos
+ * The slaves will:
+ *  -read values from an analog stick
+ *  -convert these values into a direction
+ *  -send direction values to the master
+ *  
+ *  Created by Alex Pieczynski, Ryan Fogarty, and Diego Vega
+ */
+
+#define STICKX 1
+#define STICKY 2
+
+void setup()
+{
+}
+
+void loop()
+{
+}
