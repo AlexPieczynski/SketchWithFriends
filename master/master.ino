9e7b27ff9d4cf3c63bcdba17c9f9e9e4f4fe7677
Alex Pieczynski <apiecz2@uic.edu>
2016-10-17 20:17:22 -0500

    skeleton code created for master and slave

--- master/master.ino
+++ master/master.ino
@@ -5,16 +5,37 @@
  *  -draw to an LCD screen using this input
  *  
  *  Created by Alex Pieczynski, Ryan Fogarty, and Diego Vega
+ *  
+ *  TODO - shift Direction enum to slave side, convert to Direction there. Then send 1 byte of direction.
  */
+
+#include <Wire.h>
 
 #define USR1 1
 #define USR2 2
 
 void setup()
 {
-
+  Wire.begin();
+  Serial.begin(9600);
 }
+
+enum Direction
+{
+  north, neast, east, seast, south, swest, west, nwest
+};
+
+int cursor1x = 0;
+int cursor1y = 0;
+int cursor2x = 0;
+int cursor2y = 0;
 
 void loop()
 {
+  // get two bytes from device 0. We must standardize the device addresses somewhere.
+  Wire.requestFrom(0, 1);
+  Direction dir1 = Wire.read();
+
+  Wire.requestFrom(1, 1);
+  Direction dir2 = Wire.read();
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -8,13 +8,51 @@
  *  Created by Alex Pieczynski, Ryan Fogarty, and Diego Vega
  */
 
+#include <Wire.h>
+
 #define STICKX 1
 #define STICKY 2
+#define SLAVE_ADDR 8 // these will probably need to be different for each slave
+
+enum Direction
+{
+  north, neast, east, seast, south, swest, west, nwest, none
+};
 
 void setup()
 {
+  pinMode(STICKX, OUTPUT);
+  pinMode(STICKY, OUTPUT);
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDR);
+}
+
+Direction getDirection(byte x, byte y)
+{
+  if (x == 0 && y == 0)
+    return none;
+  return 1;
+}
+
+/**
+ * This function executes whenever the master
+ *  asks for input
+ */
+void requestEvent()
+{
+  byte x = map(analogRead(STICKX), 0, 1023, 0, 3);
+  delay(1); // delay needed in between two analog reads, see https://www.arduino.cc/en/Tutorial/JoyStick
+  byte y = map(analogRead(STICKY), 0, 1023, 0, 3);
+  Serial.print("X: ");
+  Serial.println(x);
+  Serial.print("Y: ");
+  Serial.println(y);
+
+  Direction dir = getDirection(x,y);
+  Wire.write(dir);
 }
 
 void loop()
 {
+  delay(100); // idk, saw it here https://www.arduino.cc/en/Tutorial/MasterReader
 }
