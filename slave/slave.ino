d3d95acc26b03aa963829230b7867d338fce9f0b
Ryan Fogarty <rfogar2@uic.edu>
2016-10-22 11:39:49 -0500

    added joystick click interrupt

--- slave/slave.ino
+++ slave/slave.ino
@@ -12,6 +12,7 @@
 
 #define STICKX 1
 #define STICKY 2
+#define INTERRUPT_CLICK 2 // interrupt can use digital pins 2 or 3
 #define SLAVE_ADDR 8 // these will probably need to be different for each slave
 
 enum Direction
@@ -23,48 +24,14 @@ void setup()
 {
   pinMode(STICKX, INPUT);
   pinMode(STICKY, INPUT);
+  pinMode(INTERRUPT_CLICK, INPUT_PULLUP);
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDR);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_CLICK), joystickClicked, FALLING);
 }
 
 Direction getDirection(byte x, byte y)
 {
-  if (x == 0 && y == 0)
-    return none;
-  return 1;
-}
-
-/**
- * This function executes whenever the master
- *  asks for input
- */
-void requestEvent()
-{
-  byte x = map(analogRead(STICKX), 0, 1023, 0, 2);
-  delay(1); // delay needed in between two analog reads, see https://www.arduino.cc/en/Tutorial/JoyStick
-  byte y = map(analogRead(STICKY), 0, 1023, 0, 2);
-  Serial.print("X: ");
-  Serial.println(x);
-  Serial.print("Y: ");
-  Serial.println(y);
-
-  Direction dir = getDirection(x,y);
-  Wire.write(dir);
-}
-
-// custom mapping
-int cMap(int rawVal) {
-  if (rawVal < 400) {
-    return 0;
-  } else if (rawVal < 600) {
-    return 1;
-  } else {
-    return 2;
-  }
-}
-
-// TODO: think of something more clever for this
-int getDir (int x, int y) {
   if (x == 1 && y == 1) {
     return none;
   } else if (x == 1 && y == 2) {
@@ -86,10 +53,46 @@ int getDir (int x, int y) {
   }
 }
 
+/**
+ * This function executes whenever the master
+ *  asks for input
+ */
+void requestEvent()
+{
+  byte x = map(analogRead(STICKX), 0, 1023, 0, 2);
+  delay(1); // delay needed in between two analog reads, see https://www.arduino.cc/en/Tutorial/JoyStick
+  byte y = map(analogRead(STICKY), 0, 1023, 0, 2);
+  Serial.print("X: ");
+  Serial.println(x);
+  Serial.print("Y: ");
+  Serial.println(y);
+
+  Direction dir = getDirection(x,y);
+  Wire.write(dir);
+}
+
+// custom mapping
+byte cMap(int rawVal)
+{
+  if (rawVal < 400) {
+    return 0;
+  } else if (rawVal < 600) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
+void joystickClicked()
+{
+  Serial.println("Clicked");
+}
+
 void loop()
 {
-  int x = cMap(analogRead(STICKX));
-  int y = cMap(analogRead(STICKY));
-  Serial.println(getDir(x, y));
-  delay(100); // idk, saw it here https://www.arduino.cc/en/Tutorial/MasterReader
+  byte x = cMap(analogRead(STICKX));
+  byte y = cMap(analogRead(STICKY));
+  Direction dir = getDirection(x, y);
+  Serial.println(dir);
+  delay(100);
 }
